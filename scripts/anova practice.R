aa39fe28e3e70ee4c7009b5d85d1d8c057e5f190
FelixPate <jyh21yju@uea.ac.uk>
2023-03-03 13:14:13 +0000

    Initial commit

---
+++ scripts/anova practice.R
@@ -0,0 +1,12 @@
+#personal access token  = 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
+
+#loading packages
+library(tidyverse)
+library(rstatix)
+library(performance)
+library(janitor)
+library(patchwork)
+
+#loading data
+frogs <- read_csv ("data/frogs_messy_data.csv")
+
